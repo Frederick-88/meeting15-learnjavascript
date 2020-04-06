f9ce06505de691ea17ddecf536c1ecfa351cbb09
Frederick-88 <frederickfd88@gmail.com>
2020-04-06 09:46:39 +0700

    update

---
+++ script.js
@@ -0,0 +1,19 @@
+// contoh NaN
+const nilaiNaN = 5 * 'a';
+console.log(`NilaiNaN adalah ${nilaiNaN}`);
+// NaN adalah not a number
+
+// contoh undefined
+let nilaiUndefined;
+console.log(`nilai dari variable ini adalah ${nilaiUndefined}`);
+// undefined artinya nilai yang belum diberikan/diisi
+const siswa = {}
+
+// contoh null
+let nilaiNull = null;
+console.log(`nilai dari variable nilaiNull adalah ${nilaiNull}`);
+
+// string object
+const nama = "river";
+console.log(nama);
+console.log(`panjang variable nama adalah ${nama.length}`);
