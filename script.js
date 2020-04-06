dd9e3207bf0dc5c0b6130730b5fdcc1e083ec2e3
Frederick-88 <frederickfd88@gmail.com>
2020-04-06 10:53:43 +0700

    update

--- script.js
+++ script.js
@@ -43,6 +43,50 @@ let namaPanjang = `${nama} Huang`;
 console.log(namaPanjang);
 console.log(namaPanjang.toUpperCase());
 
+// contoh searching for a substring
+const kalimat ="Saya ingin jago koding supaya masuk perusahaan singapore"
+const cariKata = "perusahaan singapore";
+const kataDidapat = kalimat.indexOf(cariKata)
+if (kataDidapat >= 0){
+    console.log(`kata ${cariKata} ditemukan bro`);
+}
+else{
+    console.log(`kata ${cariKata} tidak ditemukan`);
+}
+
+// contoh special character untuk enter dengan backslash
+console.log('newline \n \n \n second newline');
+console.log('dia berkata: "memang benar itu :)" \n yang lain i can\'t stop on this ');
+
+// LEARN NUMBER
+const num = 3.14;
+console.log(num.toFixed(1));
+
+// LEARN ARRAY - BUAT ARRAY
+const namaSiswaImpactByte = ['Frederick','Ozy','Indra'];
+// const arrayKosong= [];
+let namaTambahan = "Evan"
+let fungsiTambah = namaSiswaImpactByte.push(namaTambahan);
+console.log(namaSiswaImpactByte);
+
+// PANGGIL ISI
+for(let index in namaSiswaImpactByte){
+    console.log(namaSiswaImpactByte[index]);
+}
+// PANGGIL NOMOR
+for(let nomor in namaSiswaImpactByte){
+    console.log(nomor);
+}
+
+// LEARN ARRAY METHOD
+const namaSiswaUIB = ['Agnes','Alvi','Vincent','Bill'];
+const namaSiswaGlints = [namaSiswaUIB,namaSiswaImpactByte]
+
+
+
+
+
+
 
 
 
